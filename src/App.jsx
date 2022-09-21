b04b86ed66fe996871fbc3ccc85de0e4670ebad0
lucaspericles <lucaspericles.fp@gmail.com>
2022-09-21 16:27:21 -0300

    criação dos componentes da lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,120 @@
+import {
+  Flex,
+  Box,
+  Center,
+  FormControl,
+  Input,
+  FormLabel,
+  HStack,
+  RadioGroup,
+  Radio,
+  Button,
+} from "@chakra-ui/react";
+
 function App() {
 
   return (
-    <h2>HELLO WORLD</h2>
+    <Box h="100vh">
+    <Center
+        as="header"
+        h={150}
+        bg="purple.700"
+        color="white"
+        fontWeight="light"
+        fontSize="4xl"
+        pb="8"
+      >
+        c o n t a t o
+      </Center>
+      <Flex
+        align="center"
+        justify="center"
+        bg="blackAlpha.200"
+        h="calc(100vh - 150px)"
+      >
+        <Center
+          w="100%"
+          maxW={840}
+          bg="white"
+          top={100}
+          position="absolute"
+          borderRadius={5}
+          p="6"
+          boxShadow="0 1px 2px #ccc"
+        >
+          <FormControl display="flex" flexDir="column" gap="4">
+            <HStack spacing="4">
+              <Box w="100%">
+                <FormLabel htmlFor="nome">nome</FormLabel>
+                <Input id="nome" />
+              </Box>
+              <Box w="100%">
+                <FormLabel htmlFor="email">e-mail</FormLabel>
+                <Input id="email" type="email" />
+              </Box>
+            </HStack>
+            <HStack spacing="4">
+              <Box w="100%">
+                <FormLabel htmlFor="nasc">data de nascimento</FormLabel>
+                <Input id="nasc" type="date" />
+              </Box>
+              <Box w="100%">
+                <FormLabel htmlFor="natural">naturalidade</FormLabel>
+                <Input id="natural" />
+              </Box>
+            </HStack>
+            <HStack spacing="4">
+              <Box w="100%">
+                <FormLabel htmlFor="cel">celular</FormLabel>
+                <Input id="cel" type="number" />
+              </Box>
+              <Box w="100%">
+                <FormLabel htmlFor="Tefone">telefone</FormLabel>
+                <Input id="Tefone" type="number" />
+              </Box>
+            </HStack>
+            <HStack spacing="4">
+              <Box w="100%">
+                <FormLabel htmlFor="endereco">endereço</FormLabel>
+                <Input id="endereco" />
+              </Box>
+              <Box w="100%">
+                <FormLabel htmlFor="cidade">cidade</FormLabel>
+                <Input id="cidade" />
+              </Box>
+            </HStack>
+            <HStack spacing="4">
+              <Box w="100%">
+                <FormLabel>gênero</FormLabel>
+                <RadioGroup defaultValue="Masculino">
+                  <HStack spacing="24px">
+                    <Radio value="Masculino">masculino</Radio>
+                    <Radio value="Feminino">feminino</Radio>
+                    <Radio value="Outro">outro</Radio>
+                  </HStack>
+                </RadioGroup>
+              </Box>
+            </HStack>
+            <HStack justify="center">
+              <Button
+                w={240}
+                p="6"
+                type="submit"
+                bg="purple.700"
+                color="white"
+                fontWeight="light"
+                fontSize="xl"
+                mt="2"
+                _hover={{ bg: "teal.800" }}
+              >
+                enviar
+              </Button>
+            </HStack>
+          </FormControl>
+        </Center>
+      </Flex>
+      </Box>
+      
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
+import * as React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
